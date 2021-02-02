d661c398519c1ed9f354b53a0afccd92085064db
Brandon Counts <bran.counts1@gmail.com>
2021-02-01 19:59:23 -0500

    googlz cool stuff!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import { Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import Channels from "./components/Channels";
 import Header from "./components/Header";
-import { auth, provider } from "./firebase";
+import db, { auth as firebaseAuth, provider } from "./firebase";
+import Login from "./pages/Login";
 import { logInUserRedux } from "./redux/actions/authActions";
 
-function App({ ui, logInUser }) {
-  const login = () => {
-    auth.onAuthStateChanged(user => logInUser(user));
-    auth.signInWithPopup(provider).then(user => logInUser(user)).catch(err => alert(err))
+function App({ auth, logInUser }) {
+
+
+  useEffect(() => {
+    console.log('auth is changing....', auth.user)
+  }, [auth.user]);
+  const [isLoggedIn, setLoggedIn] = useState(firebaseAuth.currentUser);
+
+  const handleLogin = (user) => {
+    logInUser(user);
+    setLoggedIn(true);
   }
-   return (
-    <div style={{backgroundColor: '#212121', height: '100vh', width: '100%'}}> 
-    <Header/>
-    <Channels />
-    <Button style={{ position: 'absolute', top: '25%', left: '50%'}} onClick={login}> Log in</Button>
+
+  const logout = () => {
+
+    logInUser({});
+    firebaseAuth.signOut().then(user => setLoggedIn(false));
+  }
+
+  return (
+    <div style={{ backgroundColor: "#212121", height: "100vh", width: "100%" }}>
+      {isLoggedIn ? (
+        <>
+          <Header />
+          <Channels />
+          <Button
+            style={{ position: "absolute", top: "25%", left: "50%" }}
+            onClick={logout}
+          >
+            {" "}
+            Log out
+          </Button>
+        </>
+      ) : (
+        <Login handleLogin={handleLogin} />
+      )}
     </div>
   );
 }
 
 const mapStateToProps = (state) => ({
-  ui: state.ui
+  auth: state.auth,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   logInUser: dispatch(logInUserRedux),
-})
-
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/pages/Login.js
@@ -0,0 +1,32 @@
+import { Button, Typography } from "@material-ui/core";
+import { connect } from "react-redux";
+import { auth, provider } from "../firebase";
+import { logInUserRedux } from "../redux/actions/authActions";
+
+const Login = ({ handleLogin }) => {
+  const login = () => {
+
+    auth
+      .signInWithPopup(provider)
+      .then((user) => handleLogin(user))
+      .catch((err) => alert(err));
+  };
+  return (
+    <div style={{ display: "grid", alignItems: "center" }}>
+      <Typography variant="h3"> You need to log in</Typography>
+      <Button
+        style={{ position: "absolute", top: "25%", left: "50%" }}
+        onClick={login}
+      >
+        {" "}
+        Log in
+      </Button>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    logInUser: dispatch(logInUserRedux),
+  })
+  
+export default connect(null, mapDispatchToProps)( Login);
